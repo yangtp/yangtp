754dc5cb051a283ede0a8e00b565c26d309876a9
杨天鹏 <1009622981@qq.com>
2012-12-28 16:13:35 +0800

    yangtp修改

---
+++ Chaper02_HelloWorld/gen/com/amaker/ch02/app/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.amaker.ch02.app;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Chaper02_HelloWorld/src/com/amaker/ch02/app/Chaper02_HelloWorldActivity.java
@@ -0,0 +1,13 @@
+package com.amaker.ch02.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Chaper02_HelloWorldActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
